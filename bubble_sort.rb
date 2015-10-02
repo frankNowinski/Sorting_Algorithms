d9c932e77c1aa41140348ccb24a43be2eb981ae3
frankNowinski <nowinski.frank@gmail.com>
2015-10-02 19:16:09 -0400

    Update bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,29 +2,22 @@
 # Sorts in O(n^2) time
 
 def bubble_sort (array)
-len = array.length 
+  len = array.length 
 
-	return array if len <= 1 # Only one element in array
+  return array if len <= 1 # Only one element in array
 
-	swapped = true
-
-	# Iterate through array until you don't swap any elements
-	while swapped == true
-		swapped = false
-		(len-1).times do |x|
-			if array[x] > array[x+1]
-				array[x], array[x+1] = array[x+1], array[x] # Swap elements
-				swapped = true
-			end
-		end
-	end
-	array
+  swapped = true
+  # Iterate through array until you don't swap any elements
+  while swapped
+    swapped = false
+    (len-1).times do |x|
+      if array[x] > array[x+1]
+      array[x], array[x+1] = array[x+1], array[x] # Swap elements
+      swapped = true
+      end
+    end
+  end
+  array
 end
-
-
-
-
-
-
 
 
