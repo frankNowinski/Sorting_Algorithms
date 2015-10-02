c83558908ba2c7cdf3f28118489553e3bc10a115
frankNowinski <nowinski.frank@gmail.com>
2015-10-02 19:27:19 -0400

    Update merge_sort.rb

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,41 +1,27 @@
+# Sample mergesort algorithm that sorts in 0(n log(n))
+
 def merge_sort(array)
-	len = array.length
+  len = array.length
 
-	return array if len <= 1
+  return array if len <= 1
 
-	mid = len / 2
-	left = array[0, mid]
-	right = array[mid, len]
-	merge(merge_sort(left), merge_sort(right))
+  # Split array in two 
+  mid = len / 2
+  left = array[0, mid]
+  right = array[mid, len]
+  merge(merge_sort(left), merge_sort(right)) 
 end
 
 def merge (left, right)
-	sorted = []
+  sorted = []
 
-	until left.empty? || right.empty?
-		if left.first <= right.first
-			sorted << left.shift
-		else
-			sorted << right.shift
-		end
-	end
-	sorted.concat(left).concat(right)
+  # Append smallest element to the sorted array
+  until left.empty? || right.empty?
+    if left.first <= right.first
+      sorted << left.shift
+    else
+      sorted << right.shift
+    end
+  end
+  sorted.concat(left).concat(right)
 end
-
-array = [1,2,3,4,5,6,7,8,9,10].shuffle
-
-puts merge_sort(array)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
